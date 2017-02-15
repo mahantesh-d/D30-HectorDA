6f3315d2943b6ab5729303c3cb68908eb355389f
Akshay Suresh <akshay.suresh@unotechsoft.com>
2017-02-15 12:50:05 +0000

    Integrate Graceful shutdown using named pipes

--- constant/hector.go
+++ constant/hector.go
@@ -1,5 +1,8 @@
 package constant
 
+// named pipe config
+const HECTOR_PIPE = "/tmp/hector"
+
 // server modes
 const HECTOR_GRPC_MODE string = "grpc"
 const HECTOR_PROTOBUF_MODE string = "protobuf"
--- servers/entry_point.go
+++ servers/entry_point.go
@@ -5,6 +5,9 @@ import (
 	"github.com/dminGod/D30-HectorDA/model"
 	"github.com/dminGod/D30-HectorDA/logger"
 	"github.com/dminGod/D30-HectorDA/constant"
+	"github.com/dminGod/D30-HectorDA/utils"
+	"os"
+	"syscall"
 )
 
 var serverType string
@@ -14,6 +17,9 @@ var RequestAbstract model.RequestAbstract
 
 // Used to start a TCP server
 func Server(serverTypePassed string) {
+
+	// call a named pipe to listen for graceful shutdown
+	go NamedPipe()
 
 	// Set the server Type
 	serverType = serverTypePassed
@@ -28,4 +34,31 @@ func Server(serverTypePassed string) {
 	} else if serverType == constant.HECTOR_GRPC_MODE {
 		GRPCStartServer()
 	}
+}
+
+func NamedPipe() {
+
+	// remove any pipe if exists
+	os.Remove(constant.HECTOR_PIPE)
+
+	// create a named pipe	
+	err := syscall.Mkfifo(constant.HECTOR_PIPE, 0666)
+
+	logger.Write("INFO", "Listening for Graceful shutdown")
+	
+	if err != nil {
+		logger.Write("INFO","Error creating named pipe" + err.Error())
+		utils.Exit(1)
+	}
+
+	file, err := os.OpenFile(constant.HECTOR_PIPE, os.O_CREATE, os.ModeNamedPipe)
+
+	if err != nil {
+		logger.Write("INFO","Error listening for named pipe" + err.Error())
+		utils.Exit(1)
+	}
+
+	logger.Write("INFO", "Graceful shutdown")
+	file.Close()
+	utils.Exit(0)
 }
