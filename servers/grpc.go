b40ec24880ac32f8c3ccbcb97a02729d702d35a5
Dushyant <dmindesigns@gmail.com>
2017-04-19 14:16:35 +0700

    Changed the d20 to d21, added a build

--- servers/grpc.go
+++ servers/grpc.go
@@ -39,7 +39,7 @@ func (g *GRPCServer) Do(ctx context.Context, req *pb.Request) (*pb.Response, err
 }
 
 // GetStream : empty stub
-func (g *GRPCServer) GetStream(req *pb.Request, streamResp pb.D20_GetStreamServer) error {
+func (g *GRPCServer) GetStream(req *pb.Request, streamResp pb.D21_GetStreamServer) error {
 
 	return nil
 }
@@ -90,7 +90,7 @@ func GRPCStartServer() {
 	}
 
 	grpcServer := grpc.NewServer()
-	pb.RegisterD20Server(grpcServer, new(GRPCServer))
+	pb.RegisterD21Server(grpcServer, new(GRPCServer))
 	grpcServer.Serve(listener)
 }
 
