f20441f7f794a7fb033e98c8078a7b62a613c780
Dushyant <dmindesigns@gmail.com>
2017-04-20 16:11:40 +0700

    Changed to add the UID field all the time for all GRPC requests and added some code for handling etcd requests but its wip

--- constant/etcd.go
+++ constant/etcd.go
@@ -1,10 +1,11 @@
 package constant
 
 // EtcdConnectionURL is the Connection URL to fetch configuration from etcd
-const EtcdConnectionURL string = "http://localhost:2379"
+const EtcdConnectionURL string = "http://10.138.32.217:2379"
 
 // EtcdEndpoints is the list of contact points in the etcd cluster
-var EtcdEndpoints = []string{"http://localhost:2379"}
+var EtcdEndpoints = []string{ "http://10.138.32.217:2379","http://10.138.32.218:2379", "http://10.138.32.219:2379",
+	"http://10.138.32.220:2379","http://10.138.32.221:2379","http://10.138.32.222:2379",  }
 
 // EtcdKey is the Key within etcd server which contains the configuration information
 const EtcdKey string = "/hector/config/config.toml"
